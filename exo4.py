ddbf8778695967c65e550b728d228933c863d17d
aminehmt <bobog330@gmail.com>
2024-12-13 18:07:57 +0100

    exo4

---
+++ exo4.py
@@ -0,0 +1,10 @@
+age1=int(input("Veuillez entrer l'age de la 1ere perssone :"))
+age2=int(input("Veuillez entrer l'age de la 2rmr personne :"))
+if age1<age2:
+    print(f"Le plus grand age est :{age2} ")
+else:
+     if age2<age1:
+              print(f"Le plus grand age est :{age1} ")
+     else:
+           print("les 2 ages sont égaux")
+ 
